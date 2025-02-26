cc8432e3080cb3076ae72d9e508d0f5330d49e5b
Gwillis442 <gwillis501@gmail.com>
2025-02-26 09:10:19 -0800

    added calculations to stereo.cc

--- stereoTest.cc
+++ stereoTest.cc
@@ -17,6 +17,27 @@ double std_dev(double x, double y) {
   return sqrt(((x - mean) * (x - mean) + (y - mean) * (y - mean)) / 2);
 }
 
-int main() {
-  cout << "Standard deviation: " << std_dev(1,1) << endl;
+void correlationCoeffecient(float* x,int rowX,int colX, float* y, int rowY, int colY, int n, float* c){
+
+  int prod = 0;
+  int sqX = 0;
+  int sqY = 0;
+  int mX = 0;
+  int mY = 0;
+
+  for(int i = 0; i < n; i++){
+      sqX += x[rowX * colX + i] * x[rowX * colX + i];
+      sqY += y[rowY * colY + i] * y[rowY * colY + i];
+
+      mX += x[rowX * colX + i];
+      mY += y[rowY * colY + i];
+
+      prod = x[rowX * colX + i] * x[rowY * colY + i];
+  }
+
+  sqX = sqX/n;
+  sqY = sqY/n;
+  mX = mX/n;
+  mY = mY/n;
+
 }
