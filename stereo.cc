4f27fe7ce1181eb2078e5a79ccf0ee92701f1d3c
gwillis442 <gwillis501@gmail.com>
2025-03-03 08:38:52 -0800

    commiting stereo.cc

--- stereo.cc
+++ stereo.cc
@@ -13,7 +13,7 @@ using namespace std;
 
 
 double correlationCoeffecient(float* x,int rowX,int colX, float* y, int rowY, int colY, int n){
-
+    //initalize variables
     int prod = 0;
     int sqX = 0;
     int sqY = 0;
@@ -25,27 +25,38 @@ double correlationCoeffecient(float* x,int rowX,int colX, float* y, int rowY, in
     double stdDevY = 0.0;
 
     for(int i = 0; i < n; i++){
+        //find the squares of both x and y
         sqX += x[rowX * colX + i] * x[rowX * colX + i];
         sqY += y[rowY * colY + i] * y[rowY * colY + i];
 
+        //find the sum of x and y
         mX += x[rowX * colX + i];
         mY += y[rowY * colY + i];
 
+        //find the product of x and y
         prod += x[rowX * colX + i] * y[rowY * colY + i];
     }
 
+    //find the mean of x and y squared
     sqX = sqX/n;
     sqY = sqY/n;
+
+    //find the mean of both mX and mY
     mX = mX/n;
     mY = mY/n;
     
+    //find the convergence 
     cov = (prod/n) - (mX*mY);
 
+    //find the standard deviantion
     stdDevX = sqrt(sqX - (mX*mX));
     stdDevY = sqrt(sqY - (mY*mY));
 
+    //find and return the correlation coefficent
     coeff = cov/(stdDevX*stdDevY);
 
+    //coefficient close to 1 or -1 are similar closer to 0 is not similar
     return coeff;
+    
 
 }
