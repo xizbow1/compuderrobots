e7c7eb0be55dd1a8eb7d8703c9ce1931c2d5c6f2
gwillis442 <gwillis501@gmail.com>
2025-03-14 08:50:18 -0700

    added pCalib and pPose

--- openCameras.sh
+++ openCameras.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Starting Left Camera
-echo "Opening Left Camera (rotated)"
+echo "Opening Left Camera"
 gnome-terminal -- bash -c "nvgstcapture-1.0 --sensorid=0 --orientation 2; exec bash"
 
 # Let Camera 1 start up
 sleep 2
 
 # Starting Right Camera 
-echo "Opening Right Camera  (rotated)"
+echo "Opening Right Camera"
 gnome-terminal -- bash -c "nvgstcapture-1.0 --sensorid=1 --orientation 2; exec bash"
 
 
