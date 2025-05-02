674b49d8bdcda7a5931d860be1c99ed74683daa6
gwillis442 <gwillis501@gmail.com>
2025-05-02 09:51:58 -0700

    syntax

--- gpuStereo/stereoKernel.h
+++ gpuStereo/stereoKernel.h
@@ -1,12 +1,13 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <cuda_runtime.h>
 #include <limits.h>
  
- __global__ void stereoKernel(unsigned char* left, unsigned char* right,
+using namespace cv;
+using namespace std;
+
+__global__ void stereoKernel(unsigned char* left, unsigned char* right,
                               unsigned char* depth, double maxDistance, 
                               int rows, int cols);
 
