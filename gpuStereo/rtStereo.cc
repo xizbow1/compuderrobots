5bad84c34cb35183d5cf0b00a668adcd3bceb11b
gwillis442 <gwillis501@gmail.com>
2025-05-03 15:03:34 -0700

    changed values

--- gpuStereo/rtStereo.cc
+++ gpuStereo/rtStereo.cc
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 // Dispaly Parameters
 int fps = 60; // in frames per sec
 int frameDelay = 1000/(2*fps); // in millisec 
-double maxDistance = 750.0; // mm
+double maxDistance = 10.0; // mm
 int rows  = 480;
 int cols  = 640;
 Mat depthImage = Mat::zeros(rows,cols, CV_8UC1);
