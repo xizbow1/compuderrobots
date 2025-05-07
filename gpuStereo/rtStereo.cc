911840cf47b2ab28b0f72ab7012a9eca34f2e0dd
gwillis442 <gwillis501@gmail.com>
2025-05-07 10:02:48 -0700

    readded print statements

--- gpuStereo/rtStereo.cc
+++ gpuStereo/rtStereo.cc
@@ -247,7 +247,7 @@ for(int row = 0; row < rows; row++){
     zone4Count = countNonZero(roi_zone4);
     */
 
-    //printf("zone0: %d, zone1: %d, zone2: %d, zone3: %d, zone4: %d\n", zone0Count, zone1Count, zone2Count, zone3Count, zone4Count);
+    printf("zone0: %d, zone1: %d, zone2: %d, zone3: %d, zone4: %d\n", zone0Count, zone1Count, zone2Count, zone3Count, zone4Count);
 
     // Determine if zone count is above threshold
     if(zone0Count > obstacleThreshold) zone0Clear = false;
@@ -296,7 +296,7 @@ for(int row = 0; row < rows; row++){
     }
 
     // Write to serial port the driving commands
-    //printf("STR: %s, Move: %s\n", strCmd, moveCmd);
+    printf("STR: %s, Move: %s\n", strCmd, moveCmd);
     bytesWritten = serialPortWrite(moveCmd,portID);
     bytesWritten = serialPortWrite(strCmd,portID);
 
