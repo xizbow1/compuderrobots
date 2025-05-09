0dba514c43bc89110dce089774ff7de0e3ce40b6
gwillis442 <gwillis501@gmail.com>
2025-05-09 11:15:09 -0700

    changed values

--- gpuStereo/stereoKernel.cu
+++ gpuStereo/stereoKernel.cu
@@ -17,9 +17,9 @@ int row = blockIdx.y*blockDim.y + threadIdx.y;
     const int halfWindow = (windowWidth-1)/2;
     int disparityStep = 2;
     int windowStep = 2;
-    double maxDisparity = 56.5;
+    double maxDisparity = 57.5;
     double contrast;
-    double contrastThreshold = 7;
+    double contrastThreshold = 3;
     
     unsigned char leftPixel;
     unsigned char rightPixel;
